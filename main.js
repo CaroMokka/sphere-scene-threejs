6d8eddff921bd399f33dc5ad0a1d9aeac4ac6013
carolina <ci.arayagonzalez@gmail.com>
2024-05-10 00:07:21 -0400

    add animations scene transition

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import './style.css'
 
@@ -6,7 +7,7 @@ import './style.css'
 const scene = new THREE.Scene();
 
 const geometry = new THREE.SphereGeometry(3,64,64);
-const material = new THREE.MeshStandardMaterial({ color: '#00ff83' });
+const material = new THREE.MeshStandardMaterial({ color: '#00edff' });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -26,14 +27,20 @@ camera.position.z = 20;
 scene.add(camera);
 
 
-
+//render
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
-
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
 renderer.render(scene,camera);
 
 const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
 
 //resize
 window.addEventListener("resize", () => {
@@ -48,7 +55,14 @@ window.addEventListener("resize", () => {
   
 });
 const loop = () => {
+  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(loop);
 }
 loop();
+
+//annimations
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
